cccb842e6d4da4ed4d7f4ab91aec22f12b604f92
chandan99342 <kumarchandanr7@gmail.com>
2023-07-25 04:27:54 +0530

    firdt challenge

---
+++ crud 1/file/about.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+   
+
+</head>
+
+<?php include 'menu.php'; ?>
+
+<section class="my-5">
+    <div class="py-5 ">
+        <h2 class="text-center">About us</h2>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-12">
+                <img src="../image/food1.jpg" class="img-fluid aboutimg">
+            </div>
+            <div class="col-lg-6 col-md-6 col-12">
+                <h2 class="display-4">I am chandan singh kushwaha</h2> 
+                <p class="py-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ea numquam natus? Reprehenderit quisquam quae perspiciatis et odit magni accusamus animi ducimus, tempora atque repudiandae mollitia, assumenda, repellat libero? Numquam?</p>   
+                <a href="about.php" class="btn btn-success">check More</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer>
+    <p class="p-3 bg-dark text-white text-center">@chandansinghkushwahaproduction</p>
+</footer>
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  
+</body>
+</html>
